d84afc0dd8c23bd2106b0b35ed55273cba304cad
Daaki Benjamin <richarddaaki4@gmail.com>
2023-02-11 02:38:31 +0300

    added a method to get grade points from grades

--- index.js
+++ index.js
@@ -20,5 +20,23 @@ function gradeToPoints(grade) {
     case "A+":
     case "A":
       return 5;
+    case "B+":
+      return 4.5;
+    case "B":
+      return 4;
+    case "C+":
+      return 3.5;
+    case "C":
+      return 3;
+    case "D+":
+      return 2.5;
+    case "D":
+      return 2;
+    case "E":
+      return 1.5;
+    case "E-":
+      return 1;
+    case "F":
+      return 0;
   }
 }
