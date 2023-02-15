9f7820d5fc9824b97372c168c4113fd905217fd4
Daaki Benjamin <richarddaaki4@gmail.com>
2023-02-15 20:18:12 +0300

    complete cmd

--- index.js
+++ index.js
@@ -45,17 +45,16 @@ class CgpaCalc {
     this.courseTree = academicYears;
   }
   printCourseTree() {
+    // console.log(this.courseTree);
+    // return
     console.log("...Generating tree ...");
+    var total_cus = 0;
+    var total_gp_cu_pdt = 0;
     var academicYears = Object.keys(this.courseTree);
     academicYears = academicYears.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
     for (var i = 0; i < academicYears.length; i++) {
-      console.log(
-        "\n\n_________________________________________________________________"
-      );
-      console.log(`               Academic Year ${academicYears[i]} `);
-      console.log(
-        "__________________________________________________________________"
-      );
+      console.log("\n\n");
+      console.log(`Academic Year ${academicYears[i]} `);
 
       // Get semesters
       var semesters = Object.keys(this.courseTree[academicYears[i]]);
@@ -67,24 +66,27 @@ class CgpaCalc {
         // console.log(courses);
 
         var cumulativecUs = 0;
-        var cgpa = 0;
+        var gpa,
+          cgpa = 0;
         var CumulativeGpCuPdt = 0;
-        for (var i = 0; i < courses.length; i++) {
-          var courseUnit = courses[i];
+        for (var k = 0; k < courses.length; k++) {
+          var courseUnit = courses[k];
           cumulativecUs += courseUnit.creditUnits;
           CumulativeGpCuPdt +=
             courseUnit.getGradePoint() * courseUnit.creditUnits;
           var x = "";
 
           console.log(
-            `${courseUnit.name
-              .toString()
-              .padEnd(30, " ")}${courseUnit.getMark()}${courseUnit.grade
-              } \t${courseUnit.getGradePoint()}`
+            `${courseUnit.name}   ${courseUnit.getMark()}  ${
+              courseUnit.grade
+            } ${courseUnit.getGradePoint()}`
           );
         }
-        cgpa = CumulativeGpCuPdt / cumulativecUs;
-        console.log(`***        CPA ${cgpa} `);
+        total_cus += cumulativecUs;
+        total_gp_cu_pdt += CumulativeGpCuPdt;
+        gpa = CumulativeGpCuPdt / cumulativecUs;
+        cgpa = total_gp_cu_pdt / total_cus;
+        console.log(`***        CPA ${gpa}  CGPA ${cgpa}`);
         console.log("______________________________________________");
       }
     }
@@ -119,6 +121,7 @@ class CourseUnit {
   }
   // Getters
   getMark() {
+    // console.log("getting mark...", gradeToMark(this.grade));
     if (this.mark) return this.mark;
     if (this.grade) return gradeToMark(this.grade);
   }
@@ -158,32 +161,35 @@ function gradeToPoints(grade) {
 }
 
 function gradeToMark(grade) {
-  switch (grade) {
-    case "A+":
-      return 90 + randomMark(10);
-    case "A":
-      return 80 + randomMark(10);
-    case "B+":
-      return 75 + randomMark(5);
-    case "B":
-      return 70 + randomMark(5);
-    case "C+":
-      return 65 + randomMark(5);
-    case "C":
-      return 60 + randomMark(5);
-    case "D+":
-      return 55 + randomMark(5);
-    case "D":
-      return 50 + randomMark(5);
-    case "E":
-      return 40 + randomMark(10);
-    case "E-":
-      return 0 + randomMark(30);
-    case "F":
-      return 0;
-    default:
-      return 0;
+  // console.log("grade", grade);
+  var val = 0;
+  if (grade === "A+") {
+    val = 90 + Number.parseInt(randomMark(10));
+  } else if (grade === "A") {
+    val = 80 + Number.parseInt(randomMark(10));
+  } else if (grade === "B+") {
+    val = 75 + Number.parseInt(randomMark(5));
+  } else if (grade === "B") {
+    val = 70 + Number.parseInt(randomMark(5));
+  } else if (grade === "C+") {
+    val = 65 + Number.parseInt(randomMark(5));
+  } else if (grade === "C") {
+    val = 60 + Number.parseInt(randomMark(5));
+  } else if (grade === "D+") {
+    val = 55 + Number.parseInt(randomMark(5));
+  } else if (grade === "D") {
+    val = 50 + Number.parseInt(randomMark(5));
+  } else if (grade === "E") {
+    val = 40 + Number.parseInt(randomMark(10));
+  } else if (grade === "E-") {
+    val = 0 + Number.parseInt(randomMark(30));
+  } else if (grade === "F") {
+    val = 0;
+  } else {
+    val = 0;
   }
+  // console.log("Finalmark ", val);
+  return val;
 }
 
 function markToGrade(mark) {
@@ -200,28 +206,75 @@ function markToGrade(mark) {
   return "F";
 }
 function randomMark(max) {
-  (Math.floor(Math.random() * 100) % 10) % max;
+  var val = (Math.floor(Math.random() * 100) % 10) % max;
+  // console.log("Value ", val);
+  return val;
 }
 
 var courses = [];
 
 courses.push(
   //Year one semester one
-  new CourseUnit("Foundatons of IS", 3, 77, 1, "2019/2020"),
-  new CourseUnit("Problem Solving", 3, "B+", 1, "2019/2020"),
+  new CourseUnit("Foundatons of IS    ", 3, "B+", 1, "2019/2020"),
+  new CourseUnit("Problem Solving     ", 3, "B+", 1, "2019/2020"),
   new CourseUnit("Communication skills", 4, "A", 1, "2019/2020"),
-  new CourseUnit("Computer literacy", 4, "A", 1, "2019/2020"),
-  new CourseUnit("Structured Programming", 3, "A+", 1, "2019/2020"),
+  new CourseUnit("Computer literacy   ", 4, "A", 1, "2019/2020"),
+  new CourseUnit("Structured Programmi", 3, "A+", 1, "2019/2020"),
 
   //Year one Semester two
-  new CourseUnit("System Analysis & Design", 4, "B", 2, "2019/2020"),
+  new CourseUnit("System Analysis & De", 4, "A", 2, "2019/2020"),
   new CourseUnit("Internet Programming", 4, "B+", 2, "2019/2020"),
-  new CourseUnit("Numerical Methods", 3, "A", 2, "2019/2020"),
-  new CourseUnit("Software Development principles", 4, "B", 2, "2019/2020"),
-  new CourseUnit("Database &Information MGT", 4, "B+", 2, "2019/2020"),
+  new CourseUnit("Numerical Methods   ", 3, "A", 2, "2019/2020"),
+  new CourseUnit("Software Development", 4, "B", 2, "2019/2020"),
+  new CourseUnit("Database &Informatio", 4, "B+", 2, "2019/2020"),
 
   //Year one Recess
-  new CourseUnit("Software Engineering Project 1", 5, "B+", 3, "2019/2020")
+  new CourseUnit("Software Engineering", 5, "B+", 3, "2019/2020"),
+
+  //Year TWO semester one
+  new CourseUnit("Discrete Mathematics", 3, "A", 1, "2020/2021"),
+  new CourseUnit("Artificial Intellige", 3, "B+", 1, "2020/2021"),
+  new CourseUnit("Data structures and ", 4, "A", 1, "2020/2021"),
+  new CourseUnit("Computer Networks   ", 4, "A", 1, "2020/2021"),
+  new CourseUnit("Formal Methods      ", 4, "A", 1, "2020/2021"),
+
+  //Year TWO Semester two
+  new CourseUnit("Calculus I          ", 4, "A", 2, "2020/2021"),
+  new CourseUnit("OPERATING SYSTEMS   ", 4, "A", 2, "2020/2021"),
+  new CourseUnit("OBJECT ORIENTED PROG", 4, "A+", 2, "2020/2021"),
+  new CourseUnit("NETWORKS APPLICATION", 4, "A", 2, "2020/2021"),
+  new CourseUnit("DATA COMMUNICATIONS ", 4, "B", 2, "2020/2021"),
+
+  //Year TWO Recess
+  new CourseUnit("Software Engineering", 5, "A", 3, "2020/2021"),
+
+  //Year THREE semester one
+  new CourseUnit("USER INTERFACE DESIG", 4, "A", 1, "2021/2022"),
+  new CourseUnit("EMBEDDED SYATEMS    ", 4, "A", 1, "2021/2022"),
+  new CourseUnit("REQUIREMENTS ENGINEE", 4, "B", 1, "2021/2022"),
+  new CourseUnit("OO ANALYSIS AND DESI", 4, "B", 1, "2021/2022"),
+  new CourseUnit("SOFTWARE METRICS    ", 3, "C+", 1, "2021/2022"),
+
+  //Year THREE Semester two
+  new CourseUnit("RESEARCH METHODOLOGY", 3, "A", 2, "2021/2022"),
+  new CourseUnit("DISTRIBUTED SYSTEMS ", 4, "A", 2, "2021/2022"),
+  new CourseUnit("SYSTEMS PROGRAMMING ", 4, "B+", 2, "2021/2022"),
+  new CourseUnit("ADVANCED OOP        ", 4, "A+", 2, "2021/2022"),
+  new CourseUnit("SOFTWARE ARCHITECTUR", 4, "A+", 2, "2021/2022"),
+
+  //YAEAR 3 INTERNSHIP
+  new CourseUnit("FIELD ATTACHMENT    ", 5, "A", 3, "2021/2022"),
+
+  //Year FOUR semester one
+  new CourseUnit("ENT PRINCIPLES      ", 3, "A", 1, "2022/2023"),
+  new CourseUnit("BUSINESS PROCESS MGT", 4, "A", 1, "2022/2023"),
+  new CourseUnit("PROJECT 1           ", 5, "A", 1, "2022/2023"),
+  new CourseUnit("SRT                 ", 4, "D", 1, "2022/2023"),
+
+  //Year FOUR Semester two
+  new CourseUnit("SOFTWARE PATTERNS   ", 4, "A+", 2, "2022/2023"),
+  new CourseUnit("PROJECT II          ", 5, "A+", 2, "2022/2023"),
+  new CourseUnit("IT LAWS             ", 4, "A+", 2, "2022/2023")
 );
 var cgpaCalc = new CgpaCalc(courses);
 
@@ -230,14 +283,3 @@ cgpaCalc.buildCourseTree();
 
 // console.log(cgpa, cgpaCalc.courseTree);
 cgpaCalc.printCourseTree();
-
-
-// //register service worker
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", function () {
-//     navigator.serviceWorker
-//       .register("/serviceworker.js")
-//       .then((res) => console.log("registered"))
-//       .catch((err) => console.log("not registered", err));
-//   });
-// }
